87ef2a9c230d3cb0e0a86232de87b88916fa494c
Tomas <tomasbruno5451@gmail.com>
2025-11-27 19:00:23 -0300

    Creacion de espacio de trabajo

---
+++ src/main/java/com/generaflyers/my_app_generate/MyAppGenerateApplication.java
@@ -0,0 +1,13 @@
+package com.generaflyers.my_app_generate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppGenerateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppGenerateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generaflyers/my_app_generate/MyAppGenerateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generaflyers.my_app_generate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppGenerateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
